75d2d6683169376143e1ac55898a657d937fdf8c
Aleksandar Matic <aleksbn417@hotmail.com>
2021-06-06 20:21:18 +0200

    End of Section 4

---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "Username is required!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required!")]
+        public string Password { get; set; }
+    }
+}
